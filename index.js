723067eeadd9898041740b2b0d630298b9533b1d
G4lile00 <g4lile000@gmail.com>
2021-10-18 18:44:31 -0300

    Last Update

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const interval = setInterval(() => {
-  const header = document.querySelector("div._1QVfy._2aN_u");
-  if (header) {
-    clearInterval(interval);
-    const a = document.createElement("a");
-    try {
-      const image = document.querySelector("img._2dXkT").src;
-      a.innerHTML = "Download";
-      a.href = image
-      a.download = image
-  
-    } catch {
-      const video = document.querySelector("video.Nu3Wb").src;
-      a.innerHTML = "Download";
-      a.href = video
-      a.download = video
-    
+function dwn() {
+
+  const interval = setInterval(() => {
+
+    console.log("loaded");
+    const header = document.querySelector("div._1QVfy._2aN_u");
+    if (header) {
+      clearInterval(interval)
+      const a = document.createElement("a");
+      try {
+        const image = document.querySelector("img._2dXkT").src;
+        a.innerHTML = "Download";
+        a.href = image;
+        a.download = image;
+      } catch {
+        const video = document.querySelector("video.Nu3Wb").src;
+        a.innerHTML = "Download";
+        a.href = video;
+        a.download = video;
+      }
+
+
+
+      a.classList.add("DownloadBtn");
+
+      header.appendChild(a);
+
     }
 
-    a.classList.add("DownloadBtn");
+  }, 1000);
+
+  
+}
+
+document.addEventListener(onload, dwn())
 
 
-    console.log(header)
-    
 
 
-    header.appendChild(a);
-  }
-}, 1000);
+
+
+
+
+
